978c6eac482f8c010f173951f5e5474a80927133
luiz <luiz.fernando360@gmail.com>
2023-02-18 22:35:39 -0300

    ajuste no visual do login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,35 +5,18 @@ import {AuthComponent} from './layout/auth/auth.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: AdminComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
-      }, {
-        path: 'dashboard',
-        loadChildren: () => import('./pages/dashboard/dashboard-default/dashboard-default.module').then(m => m.DashboardDefaultModule)
-      }, {
-        path: 'basic',
-        loadChildren: () => import('./pages/ui-elements/basic/basic.module').then(m => m.BasicModule)
-      },{
-        path: 'map',
-        loadChildren: () => import('./pages/map/google-map/google-map.module').then(m => m.GoogleMapModule),
-      }, {
-        path: 'user',
-        loadChildren: () => import('./pages/user/profile/profile.module').then(m => m.ProfileModule)
-      }, {
-        path: 'simple-page',
-        loadChildren: () => import('./pages/simple-page/simple-page.module').then(m => m.SimplePageModule)
-      }
+    path: '', component: AdminComponent, children: [
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'}, 
+      {path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard-default/dashboard-default.module').then(m => m.DashboardDefaultModule)}, 
+      {path: 'basic', loadChildren: () => import('./pages/ui-elements/basic/basic.module').then(m => m.BasicModule)},
+      {path: 'map', loadChildren: () => import('./pages/map/google-map/google-map.module').then(m => m.GoogleMapModule)}, 
+      {path: 'user', loadChildren: () => import('./pages/user/profile/profile.module').then(m => m.ProfileModule)}, 
+      {path: 'simple-page', loadChildren: () => import('./pages/simple-page/simple-page.module').then(m => m.SimplePageModule)}
     ]
   },
+
   {
-    path: '',
-    component: AuthComponent,
-    children: [
+    path: '', component: AuthComponent, children: [
       {
         path: 'authentication',
         loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule)
